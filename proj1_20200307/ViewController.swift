9b8344434addfc35852d8ebb45b523c05f77ebe7
dbstjqkddl <bysj413@gmail.com>
2020-03-07 15:20:38 +0900

    Initial Commit

---
+++ proj1_20200307/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proj1_20200307
+//
+//  Created by 변윤섭 on 2020/03/07.
+//  Copyright © 2020 변윤섭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
